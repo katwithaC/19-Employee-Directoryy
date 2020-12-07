ae69cad6bef3289d6b82bd8efcf47f3e7399b98a
katwithaC <catherinerosearredondo@gmail.com>
2020-12-07 00:22:55 -0600

    updating Header component index.js

--- employee-directory/src/components/Header/index.js
+++ employee-directory/src/components/Header/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Header(props) {
-
+<div className = "page-header">
+<h1>
+Employee Directory
+<small>Click on carrots to filter by heading or use the search box to narrow your results.</small>
+</h1>
+</div>
 }
 
 export default Header;
