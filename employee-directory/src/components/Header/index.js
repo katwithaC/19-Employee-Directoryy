226dc8c78033973d5578f2dfed339561ac30aeac
katwithaC <catherinerosearredondo@gmail.com>
2020-12-07 18:29:27 -0600

    adding navbar className to Header index.js

--- employee-directory/src/components/Header/index.js
+++ employee-directory/src/components/Header/index.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
 
 function Header(props) {
-<div className = "page-header">
-<h1>
-Employee Directory
-<small>Click on carrots to filter by heading or use the search box to narrow your results.</small>
-</h1>
-</div>
+    return (
+        <div className = "navbar">
+        <div>
+        <h1 className= "h1">Employee Directory</h1>
+        <small className = "small">Click on carrots to filter by heading or use the search box to narrow your search results.</small>
+        </div>
+        </div>
+        
+    )
 }
 
 export default Header;
