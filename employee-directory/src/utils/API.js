e87dea102548fea85a3d96fcaa5f03f7fbc93557
katwithaC <catherinerosearredondo@gmail.com>
2020-12-07 21:09:16 -0600

    fixing api call on API.js

--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -1,7 +1,19 @@
-import axios from "axios"
+import axios from "axios";
+
+const BASEURL = "https://randomuser.me/api/?results=200";
 
 export default {
-    getRandomEmployees: function() {
-        return axios.get("https://randomuser.me/api/?results=200");
+    search: function(query) {
+        return axios.get(BASEURL + query);
     }
 };
+
+
+
+// import axios from "axios"
+
+// export default {
+//     getRandomEmployees: function() {
+//         return axios.get("https://randomuser.me/api/?results=200");
+//     }
+// };
